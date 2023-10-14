3fc747fa59ee9c0ad4dfe123914a59e0836e36a2
ertucode <ertugrulsirtt@gmail.com>
2023-10-14 10:25:30 +0300

    ng nvm npm

--- nvm.sh
+++ nvm.sh
@@ -1,7 +1,25 @@
 #!/bin/sh
 
-export NVM_DIR="/home/ertu/.nvm"
-nvm() {
-  [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
-  nvm $@
+export NVM_DIR="$HOME/.nvm"
+
+function loadnvm () {
+    if [ "$NVM_INITTED" != "1" ]; then
+      [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+      NVM_INITTED=1
+    fi
+}
+
+nvm () {
+    loadnvm
+    command nvm "$@"
+}
+
+ng () {
+    loadnvm
+    command ng "$@"
+}
+
+npm () {
+    loadnvm
+    command npm "$@"
 }
